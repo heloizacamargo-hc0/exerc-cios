145fbd7860ee4e7ff78a51ef85327ab920c15e49
heloizacamargo-hc0 <heloiza_camargo@estudante.sesisenai.org.br>
2026-02-19 22:11:59 -0300

    terceira aula

---
+++ modelos.py
@@ -0,0 +1,22 @@
+'''
+Nome: Heloiza Camargo
+
+Classe: pedido 
+
+Atributos:
+nome_cliente
+endereço_entrega
+valor_pedido
+status_pedido
+tempo_entrega
+
+Comportamentos:
+Armazenar o nome do cliente que fez o pedido
+O endereço onde o pedido será entregue 
+O valor total do pedido do cliente
+Para o cliente saber a rota do seu pedido
+Indicar o tempo de espera do pedido
+
+'''
+
+class pedido:
